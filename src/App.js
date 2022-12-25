15ac06fe737f20b0ea6399a9c587330ee6debb09
Mofaz Shmuely <mofazshmuely69@gmail.com>
2022-12-25 20:44:21 +0200

    firstStep

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./home/home";
 
 function App() {
   useEffect(() => {
@@ -12,21 +14,10 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
