f87f65e0885870ef35ca7ca7bc3f6c40ed9937e6
Mofaz Shmuely <mofazshmuely69@gmail.com>
2022-12-29 20:53:58 +0200

    nav bar + su

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/profile">Profile</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/search">Search</Link>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -6,36 +6,100 @@ import Row from "react-bootstrap/Row";
 import "./SignUp.css";
 
 export default function SignUp() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const passwordRepeat = formData.get("passwordRepeat");
+    const firstName = formData.get("firstName");
+    const lastName = formData.get("lastName");
+    const phoneNumber = formData.get("phoneNumber");
+
+    try {
+      const response = await fetch("http://localhost:3001/user/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          password,
+          passwordRepeat,
+          firstName,
+          lastName,
+          phoneNumber,
+        }),
+      });
+      const result = await response.json();
+      if (response.statusCode !== 200) {
+        throw new Error(result.message);
+      }
+      // setUser(result)
+      // save user to userContext
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
       </Form.Group>
 
       <Form.Group as={Col} controlId="formGridPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
       </Form.Group>
 
       <Form.Group as={Col} controlId="formGridPassword2">
         <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm your Password" />
+
+        <Form.Control
+          type="password"
+          name="passwordRepeat"
+          placeholder="Confirm your Password"
+        />
       </Form.Group>
 
       <Form.Group as={Col} controlId="formGridFirstName">
         <Form.Label>First name</Form.Label>
-        <Form.Control type="name" placeholder="First name" />
+        <Form.Control
+          type="name"
+          name="firstName"
+          placeholder="First name"
+          required
+        />
       </Form.Group>
 
       <Form.Group as={Col} controlId="formGridLastName">
         <Form.Label>Last name</Form.Label>
-        <Form.Control type="name" placeholder="Last name" />
+        <Form.Control
+          type="name"
+          placeholder="Last name"
+          name="lastName"
+          required
+        />
       </Form.Group>
 
       <Form.Group as={Col} controlId="formGridPhone">
         <Form.Label>Phone number</Form.Label>
-        <Form.Control type="number" placeholder="Phone number" />
+        <Form.Control
+          type="number"
+          placeholder="Phone number"
+          name="phoneNumber"
+          required
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit">
